e7749bd3d00a07d2f311b30a63a910d52ce2f273
lidiabradea <lidia_bradea@yahoo.com>
2024-03-06 14:14:39 +0200

    created database class

---
+++ Database.php
@@ -0,0 +1,29 @@
+<?php
+
+use PDO;
+
+class Database
+{
+  public $conn;
+
+  /**
+   * Constructor for Database class
+   * 
+   * @param array $config
+   */
+  public function __construct($config)
+  {
+    $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}";
+
+    $options = [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    ];
+
+    try {
+      $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
+    } catch (PDOException $e) {
+      throw new Exception("Database connection failed: {$e->getMessage()}");
+    }
+  }
+}
